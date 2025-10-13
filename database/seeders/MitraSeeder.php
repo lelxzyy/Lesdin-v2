6a095116767f32dbfac7a2b70325eb0316656722
Marvellino Apriedo <167978243+Marvellinoa@users.noreply.github.com>
2025-10-14 01:00:00 +0700

    Update MitraSeeder.php

--- database/seeders/MitraSeeder.php
+++ database/seeders/MitraSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Mitra;
 
@@ -22,15 +21,7 @@ class MitraSeeder extends Seeder
                 'alamat' => 'Jl. Gatot Subroto No. 123, Jakarta Selatan',
                 'kuota' => 10,
                 'jurusan_id' => 1,
-            ],
-            [
-                'name' => 'CV. Digital Solutions',
-                'deskripsi' => 'Perusahaan yang fokus pada solusi digital untuk berbagai industri.',
-                'kontak' => '021-8765432',
-                'email' => 'contact@digitalsolutions.co.id',
-                'alamat' => 'Jl. Sudirman No. 456, Jakarta Pusat',
-                'kuota' => 8,
-                'jurusan_id' => 2,
+                'gambar' => 'teknologinusantara.png',
             ],
             [
                 'name' => 'PT. Media Kreatif Indonesia',
@@ -40,33 +31,57 @@ class MitraSeeder extends Seeder
                 'alamat' => 'Jl. Thamrin No. 789, Jakarta Pusat',
                 'kuota' => 6,
                 'jurusan_id' => 3,
+                'gambar' => 'mediakreatif.png',
             ],
             [
-                'name' => 'Bengkel Otomotif Maju Jaya',
-                'deskripsi' => 'Bengkel spesialis kendaraan ringan dengan teknologi modern.',
-                'kontak' => '021-5432109',
-                'email' => 'majujaya@gmail.com',
-                'alamat' => 'Jl. Raya Bekasi No. 321, Bekasi',
-                'kuota' => 12,
-                'jurusan_id' => 4,
-            ],
-            [
-                'name' => 'PT. Elektronik Industri Mandiri',
-                'deskripsi' => 'Perusahaan manufaktur komponen elektronik industri.',
+                'name' => 'PT. Panasonic Manufacturing Indonesia',
+                'deskripsi' => 'Manufaktur perangkat elektronik skala nasional.',
                 'kontak' => '021-6543210',
-                'email' => 'hrd@elektronikmandiri.com',
-                'alamat' => 'Kawasan Industri Pulogadung, Jakarta Timur',
+                'email' => 'hr@panasonic.co.id',
+                'alamat' => 'Kawasan Industri Cikarang, Bekasi',
                 'kuota' => 15,
                 'jurusan_id' => 5,
+                'gambar' => 'panasonic.webp',
             ],
             [
-                'name' => 'Studio Kreatif Multimedia',
-                'deskripsi' => 'Studio produksi video, animasi, dan konten digital.',
-                'kontak' => '021-3456789',
-                'email' => 'info@studiokreatif.com',
-                'alamat' => 'Jl. Kemang Raya No. 567, Jakarta Selatan',
+                'name' => 'Jogja Techno Park (JTP)',
+                'deskripsi' => 'Inkubator teknologi, IoT, startup hub, dan pelatihan digital.',
+                'kontak' => '0274-345678',
+                'email' => 'admin@jtp.co.id',
+                'alamat' => 'Jl. Wahid Hasyim No. 47, Sleman, Yogyakarta',
                 'kuota' => 5,
-                'jurusan_id' => 3,
+                'jurusan_id' => 1,
+                'gambar' => 'jogjatechnopark.jpg',
+            ],
+            [
+                'name' => 'PT. Cargloss Pratama',
+                'deskripsi' => 'Perusahaan manufaktur helm dan cat otomotif terbesar di Indonesia.',
+                'kontak' => '021-9876500',
+                'email' => 'info@cargloss.com',
+                'alamat' => 'Jl. Raya Tajur No. 77, Bogor',
+                'kuota' => 10,
+                'jurusan_id' => 4,
+                'gambar' => 'cargloss.png',
+            ],
+            [
+                'name' => 'PT. Multi Integra',
+                'deskripsi' => 'Perusahaan IT solusi sistem informasi & integrasi jaringan.',
+                'kontak' => '0274-556677',
+                'email' => 'contact@multiintegra.co.id',
+                'alamat' => 'Jl. Solo Km 10, Sleman, Yogyakarta',
+                'kuota' => 8,
+                'jurusan_id' => 1,
+                'gambar' => 'multiintegra.png',
+            ],
+            [
+                'name' => 'PT. Nutrifood Indonesia',
+                'deskripsi' => 'Perusahaan FMCG (Tropicana Slim, Nutrisari, HiLo) dengan fokus nutrisi.',
+                'kontak' => '021-7654321',
+                'email' => 'hr@nutrifood.co.id',
+                'alamat' => 'Jl. Rawamangun No. 15, Jakarta Timur',
+                'kuota' => 6,
+                'jurusan_id' => 5,
+                'gambar' => 'nutrifood.png',
             ],
         ];
 
@@ -75,4 +90,3 @@ class MitraSeeder extends Seeder
         }
     }
 }
-
