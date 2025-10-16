d7b159fbacdff1de8db68710add53f8394f50326
lelxzyy <dalilamiudin@gmail.com>
2025-10-16 18:40:51 +0700

    jurusann

--- database/seeders/JurusanSeeder.php
+++ database/seeders/JurusanSeeder.php
@@ -2,38 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Jurusan;
 
 class JurusanSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Jalankan seeder untuk tabel jurusan.
      */
     public function run(): void
     {
         $jurusans = [
-            [
-                'nama_jurusan' => 'Teknik Komputer dan Jaringan',
-                'kode_jurusan' => 'TKJ',
-            ],
-            [
-                'nama_jurusan' => 'Multimedia',
-                'kode_jurusan' => 'MM',
-            ],
-            [
-                'nama_jurusan' => 'Teknik Kendaraan Ringan Otomotif',
-                'kode_jurusan' => 'TKRO',
-            ],
-            [
-                'nama_jurusan' => 'Teknik Elektronika Industri',
-                'kode_jurusan' => 'TEI',
-            ],
-            [
-                'nama_jurusan' => 'Rekayasa Perangkat Lunak',
-                'kode_jurusan' => 'RPL',
-            ],
+            ['nama_jurusan' => 'Sistem Informasi Jaringan Aplikasi', 'kode_jurusan' => 'SIJA'],
+            ['nama_jurusan' => 'Geologi Pertambangan', 'kode_jurusan' => 'GP'],
+            ['nama_jurusan' => 'Teknik Fabrikasi Logam Manufaktur', 'kode_jurusan' => 'TFLM'],
+            ['nama_jurusan' => 'Kimia Analisis', 'kode_jurusan' => 'KA'],
+            ['nama_jurusan' => 'Kimia Industri', 'kode_jurusan' => 'KI'],
+            ['nama_jurusan' => 'Teknik Pemesinan', 'kode_jurusan' => 'TP'],
+            ['nama_jurusan' => 'Teknik Kendaraan Ringan', 'kode_jurusan' => 'TKR'],
+            ['nama_jurusan' => 'Teknik Bodi Kendaraan Ringan', 'kode_jurusan' => 'TBKR'],
+            ['nama_jurusan' => 'Teknik Otomasi Industri', 'kode_jurusan' => 'TOI'],
+            ['nama_jurusan' => 'Teknik Instalasi Tenaga Listrik', 'kode_jurusan' => 'TITL'],
+            ['nama_jurusan' => 'Desain Permodelan Informasi Bangunan', 'kode_jurusan' => 'DPIB'],
+            ['nama_jurusan' => 'Konstruksi Gedung Sanitasi', 'kode_jurusan' => 'KGS'],
+            ['nama_jurusan' => 'Teknik Elektronika Komunikasi', 'kode_jurusan' => 'TEK'],
         ];
 
         foreach ($jurusans as $jurusan) {
